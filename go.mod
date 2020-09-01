871f2b7df25bbfcd858ed7c4eda83513fd7d7d59
suxiqiang <592364224@qq.com>
2020-09-01 11:06:43 +0800

    feat: 添加跨域请求的处理

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ginrtsp
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.5.0
 	github.com/gorilla/websocket v1.4.1
 	github.com/joho/godotenv v1.3.0
--- server/router.go
+++ server/router.go
@@ -2,13 +2,15 @@ package server
 
 import (
 	"ginrtsp/api"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // NewRouter Gin 路由配置
 func NewRouter() *gin.Engine {
 	r := gin.Default()
+	// 添加跨域请求处理
+	r.Use(cors.Default())
 
 	// 路由
 	r.GET("/ping", api.Ping)
